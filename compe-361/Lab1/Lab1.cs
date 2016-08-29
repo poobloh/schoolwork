f108c19f0a8e5c9488f889f2437693a6d669f615
Brandon Castro <Brandon.Castro@resmed.com>
2016-08-29 15:42:44 -0700

    Edited Lab1

--- compe-361/Lab1/Lab1.cs
+++ compe-361/Lab1/Lab1.cs
@@ -1,5 +1,7 @@
 /// <summary>
-/// Outputs name, dog's name, and 3 favorite games/books
+/// 1. Output your name to user
+/// 2. Output your best friend's, dog's, cat's, or nemesis' name (pick one)
+/// 3. Output your three favorite games or books (each name on seperate line)
 /// </summary>
 using System;
 
@@ -7,6 +9,11 @@ public class Lab1
 {
   public static void Main()
   {
+    Console.WriteLine("My name is Brandon Castro");
+    Console.WriteLine("My dog's name is Teddy");
+    Console.WriteLine("Name1");
+    Console.WriteLine("Name2");
+    Console.WriteLine("Name3");
     Console.WriteLine("Hello world");
   }
 }
