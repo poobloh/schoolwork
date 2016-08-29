61193d90fa08458014bddde9005d945b4af3a35c
Brandon Castro <Brandon.Castro@resmed.com>
2016-08-29 15:37:17 -0700

    Revert "no message"
    This reverts commit ed5db920ad34ebc3f19402c41e428407c2d36af1.

--- compe-361/Lab1/Lab1.cs
+++ compe-361/Lab1/Lab1.cs
@@ -1,7 +1,5 @@
 /// <summary>
-/// 1. Output your name to user
-/// 2. Output your best friend's, dog's, cat's, or nemesis' name (pick one)
-/// 3. Output your three favorite games or books (each name on seperate line)
+/// Outputs name, dog's name, and 3 favorite games/books
 /// </summary>
 using System;
 
@@ -9,10 +7,6 @@ public class Lab1
 {
   public static void Main()
   {
-    Console.WriteLine("My name is Brandon Castro");
-    Console.WriteLine("My dog's name is Teddy");
-    Console.WriteLine("Name1");
-    Console.WriteLine("Name2");
-    Console.WriteLine("Name3");
+    Console.WriteLine("Hello world");
   }
 }
